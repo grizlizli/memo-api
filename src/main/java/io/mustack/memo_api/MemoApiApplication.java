eb7c62ac9985bab9f4bfbb57a2909368c4dac146
grizlizli <mkrstic@capaciteam.com>
2024-12-26 18:58:23 +0100

    enable mongo auditing

--- src/main/java/io/mustack/memo_api/MemoApiApplication.java
+++ src/main/java/io/mustack/memo_api/MemoApiApplication.java
@@ -2,8 +2,10 @@ package io.mustack.memo_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class MemoApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/mustack/memo_api/api/model/Note.java
+++ src/main/java/io/mustack/memo_api/api/model/Note.java
@@ -1,6 +1,10 @@
 package io.mustack.memo_api.api.model;
 
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "notes")
@@ -11,6 +15,12 @@ public class Note {
     private String title;
     private String content;
     private Boolean deleted = false;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
 
     public Note(String id, String title, String content, Boolean deleted) {
         this.id = id;
@@ -49,5 +59,21 @@ public class Note {
 
     public void setDeleted(Boolean deleted) {
         this.deleted = deleted;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }

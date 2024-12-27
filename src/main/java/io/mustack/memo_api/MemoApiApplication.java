7dc9ada214356341ca8921aff999c7fc2d43c741
grizlizli <mkrstic@capaciteam.com>
2024-12-27 15:23:57 +0100

    allow cors

--- src/main/java/io/mustack/memo_api/MemoApiApplication.java
+++ src/main/java/io/mustack/memo_api/MemoApiApplication.java
@@ -2,7 +2,10 @@ package io.mustack.memo_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoAuditing
@@ -10,6 +13,16 @@ public class MemoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemoApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/notes").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }

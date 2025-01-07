c5e2cec0adfed61b5e61d62cf8e5337955d58912
grizlizli <mkrstic@capaciteam.com>
2025-01-07 20:23:49 +0100

    cleanup

--- src/main/java/io/mustack/memo_api/api/model/Note.java
+++ src/main/java/io/mustack/memo_api/api/model/Note.java
@@ -7,6 +7,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+
 @Document(collection = "notes")
 public class Note {
 

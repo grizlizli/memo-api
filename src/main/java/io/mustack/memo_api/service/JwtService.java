3f470a35d955109b6064650daf872058f9d04640
grizlizli <mkrstic@capaciteam.com>
2025-01-07 20:25:19 +0100

    jwt service

---
+++ src/main/java/io/mustack/memo_api/service/JwtService.java
@@ -0,0 +1,69 @@
+package io.mustack.memo_api.service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    // Load from config file
+    private final String SECRET_KEY = "7d706b6804dee10e1a4ad28967d954caf6037fe4e702b3dd30121ede3fc3e403";
+
+    public String generateToken(String email) {
+        String token = Jwts
+            .builder()
+            .subject(email)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000))
+            .signWith(getSigningKey())
+            .compact();
+
+        return token;
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token) {
+        String email = extractEmail(token);
+        // mock
+        return (email.equals("admin@localhost")) && !isTokenExpired(token);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
